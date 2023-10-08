436205f6667faa1920d6e0b56329dc475f824999
opushnev <opushnev.ia@phystech.edu>
2023-10-08 14:22:14 +0300

    gh-pages init

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Secure</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
