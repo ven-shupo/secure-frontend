8a958a3ac213ae0776aba3a22ac29ff70c63f8b6
opushnev <opushnev.ia@phystech.edu>
2023-10-08 14:07:18 +0300

    init commit

---
+++ lib/telegram.tsx
@@ -0,0 +1,96 @@
+import Script from "next/script";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import React from 'react';
+
+export interface ITelegramUser {
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    language_code: string;
+  }
+  
+  export interface IWebApp {
+    initData: string;
+    initDataUnsafe: {
+      query_id: string;
+      user: ITelegramUser;
+      auth_date: string;
+      hash: string;
+    };
+    version: string;
+    platform: string;
+    colorScheme: string;
+    themeParams: {
+      link_color: string;
+      button_color: string;
+      button_text_color: string;
+      secondary_bg_color: string;
+      hint_color: string;
+      bg_color: string;
+      text_color: string;
+    };
+    isExpanded: boolean;
+    viewportHeight: number;
+    viewportStableHeight: number;
+    isClosingConfirmationEnabled: boolean;
+    headerColor: string;
+    backgroundColor: string;
+    BackButton: {
+      isVisible: boolean;
+    };
+    MainButton: {
+      text: string;
+      color: string;
+      textColor: string;
+      isVisible: boolean;
+      isProgressVisible: boolean;
+      isActive: boolean;
+    };
+    HapticFeedback: any;
+  }
+
+export interface ITelegramContext {
+  webApp?: IWebApp;
+  user?: ITelegramUser;
+}
+
+export const TelegramContext = createContext<ITelegramContext>({});
+
+export const TelegramProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [webApp, setWebApp] = useState<IWebApp | null>(null);
+
+  useEffect(() => {
+    const app = (window as any).Telegram?.WebApp;
+    if (app) {
+      app.ready();
+      setWebApp(app);
+    }
+  }, []);
+
+  const value = useMemo(() => {
+    return webApp
+      ? {
+          webApp,
+          unsafeData: webApp.initDataUnsafe,
+          user: webApp.initDataUnsafe.user,
+        }
+      : {};
+  }, [webApp]);
+
+  return (
+    <TelegramContext.Provider value={value}>
+      {/* Make sure to include script tag with "beforeInteractive" strategy to pre-load web-app script */}
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="beforeInteractive"
+      />      {children}
+    </TelegramContext.Provider>
+  );
+};
+
+export const useTelegram = () => useContext(TelegramContext);
