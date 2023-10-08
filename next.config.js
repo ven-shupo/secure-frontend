b89104408f8cd310811305d1a596a2d6330304f9
opushnev <opushnev.ia@phystech.edu>
2023-10-08 17:45:41 +0300

    actions init

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-/** @type {import('next').NextConfig} */
-const repo = 'secure-frontend'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
 const nextConfig = {
-    reactStrictMode: true,
-    distDir: 'build',
-    assetPrefix: assetPrefix,
-    basePath: basePath,
+    output: "export",
+    basePath: "/secure-frontend",
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
